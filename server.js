1001dcb3ccc54b54e94cf00c4d2de522af160f20
Hasti S. Gor <hastigor123@gmail.com>
2026-06-28 18:44:54 +0530

    Modify socket.io transport configuration
    Updated socket.io transport options to include only polling.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"]
   },
-  transports: ["polling", "websocket"]
+  transports: ["polling"]   // IMPORTANT
 });
 
 /* STATIC FRONTEND */
