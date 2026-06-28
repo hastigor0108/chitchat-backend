e7a2af9f4a80d4e81aa5ab8d6360c0683b7f9960
Hasti S. Gor <hastigor123@gmail.com>
2026-06-28 15:14:25 +0530

    Updated code
    Added CORS support and a test endpoint to the server.

--- server.js
+++ server.js
@@ -4,33 +4,76 @@ const { Server } = require("socket.io");
 const path = require("path");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
+const server = http.createServer(app);
+
+/* SOCKET.IO */
+const io = new Server(server, {
+cors: {
+origin: "*",
+methods: ["GET", "POST"]
+}
+});
+
+/* FRONTEND */
 app.use(
 express.static(
 path.join(__dirname, "../frontend")
 )
 );
 
+/* TEST */
+app.get("/", (req, res) => {
+res.send("ChitChat backend running 💌");
+});
+
+/* SOCKET */
 io.on("connection", (socket) => {
 
 console.log("user connected");
 
+/* SEND MESSAGE */
 socket.on("send-message", (data) => {
-io.emit("receive-message", data);
+
+io.emit(
+"receive-message",
+data
+);
+
 });
 
+/* TYPING */
 socket.on("typing", (data) => {
-socket.broadcast.emit("typing", data);
+
+socket.broadcast.emit(
+"typing",
+data
+);
+
 });
 
+/* DISCONNECT */
 socket.on("disconnect", () => {
-console.log("user disconnected");
-});
+
+console.log(
+"user disconnected"
+);
 
 });
 
-server.listen(3000, () => {
-console.log("http://localhost:3000");
 });
+
+/* PORT */
+const PORT =
+process.env.PORT || 3000;
+
+server.listen(
+PORT,
+() => {
+
+console.log(
+`Server running on ${PORT}`
+);
+
+}
+);
